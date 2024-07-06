f2bd88c9d1b50696067f422e7214e22b35ad8106
S4SPIDEY <41261098+S4SPIDEY@users.noreply.github.com>
2024-07-06 16:11:53 +0530

    Create AlloraNodeCheck.sh

---
+++ AlloraNodeCheck.sh
@@ -0,0 +1,22 @@
+curl --location 'http://localhost:6000/api/v1/functions/execute' \
+--header 'Content-Type: application/json' \
+--data '{
+    "function_id": "bafybeigpiwl3o73zvvl6dxdqu7zqcub5mhg65jiky2xqb4rdhfmikswzqm",
+    "method": "allora-inference-function.wasm",
+    "parameters": null,
+    "topic": "1",
+    "config": {
+        "env_vars": [
+            {
+                "name": "BLS_REQUEST_PATH",
+                "value": "/api"
+            },
+            {
+                "name": "ALLORA_ARG_PARAMS",
+                "value": "ETH"
+            }
+        ],
+        "number_of_nodes": -1,
+        "timeout": 2
+    }
+}'
